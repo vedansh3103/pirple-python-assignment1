c3ef8dff8392bff688ff265b8a37610cd54793ec
VEDANSH VARSHNEY <45974030+vedansh3103@users.noreply.github.com>
2018-12-18 21:53:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# Different attributes about my favorite song
+
+Song= "Blood, Sweat and Tears"              # Name of the song
+Album= "Wings"                              # Album to which the song belongs to
+Artist= "BTS"                               # Name of the singing artists
+ReleaseYear= 2016                           # Year of release of the song
+Length= 3.37                                # Length of song in minutes.seconds
+Genre= "Pop"                                # Genre of the song
+Lang= "Korean"                              # Language of the song
+ActualName= "피 땀 눈물"                     # Name of the song in actual language
+Views= 383790274                            # Current number of views on the MV on Youtube
+Likes= 5000000                              # Number of likes on MV on Youtube
+Peak=1                                      # Peak rank on US Billboard
+Producer= "Pdogg"                           # Producer of the song
+
+FavLine= "나를 부드럽게 죽여줘 너의 손길로 눈 감겨줘"       # Favourite part of the lyrics
+Meaning= "Kill me softly, close my eyes with your touch" # Meaning of the above lyrics
+
+print(Song)
+print(Album)
+print(Artist)
+print(ReleaseYear)
+print(Length)
+print(Genre)
+print(Lang)
+print(ActualName)
+print(Views)
+print(Likes)
+print(Peak)
+print(Producer)
+print(FavLine)
+print(Meaning)
